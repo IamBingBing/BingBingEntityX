27015044e9759e4ed8878653579eb82bcc35f49d
McpePuki <60613217+McpePuki@users.noreply.github.com>
2020-03-21 22:37:35 +0900

    Update Cow.php

--- BingBingEntityX/src/Entity/Animal/Cow.php
+++ BingBingEntityX/src/Entity/Animal/Cow.php
@@ -9,7 +9,7 @@ class Cow extends BaseAnimal{
     public function __construct($name , $pos){
         parent::__construct($name , $pos);
         $nbt = Entity::createBaseNBT($this->asVector3() , $this->motion , $this->yaw , $this->pitch);
-        $this->entity = Entity::createEntity("Chicken", $this->getPosition()->getLevel(), $nbt );
+        $this->entity = Entity::createEntity("Cow", $this->getPosition()->getLevel(), $nbt );
         $this->damage = 1;
         $this->damageTick= 1.5;
         $this->damageRange =1;
