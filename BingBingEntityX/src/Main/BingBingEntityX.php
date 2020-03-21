113bb9a81e24ac26ba4372aa8bd526b80151f008
McpePuki <60613217+McpePuki@users.noreply.github.com>
2020-03-21 23:43:31 +0900

    Update BingBingEntityX.php

--- BingBingEntityX/src/Main/BingBingEntityX.php
+++ BingBingEntityX/src/Main/BingBingEntityX.php
@@ -7,11 +7,16 @@ use Entity\NPC;
 use Entity\Animal\Chicken;
 use Entity\Animal\BaseAnimal;
 
+use pocketmine\event\entity\{
+	EntityDamageEvent, EntityDamageByEntityEvent
+};
+
 class BingBingEntityX extends PluginBase implements Listener{
     private static $instance;
+    public $targetdata = [];
     
     public function onEnable(){
-        //$this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
         Entity::registerEntity(NPC::class , true);
         Entity::registerEntity(Chicken::class , true);
     }
@@ -41,6 +46,22 @@ class BingBingEntityX extends PluginBase implements Listener{
                 break;
             
                 
+        }
+    }
+    
+    public function onTargetDamage(EntityDamageEvent $event) {
+        if($event instanceof EntityDamageByEntityEvent) {
+            $entity = $event->getEntity();
+            $target = $event->getDamager();
+            if($entity instanceof Wolf && $target instanceof Player){
+                foreach(Server::getInstance()->getLevels()->getEntities() as $entity) { 
+                    if(isset($this->targetdata[$entity->getId() ) return true; //이미 늑대들이 타켓을 고르면 다른사람이 떄려도 타켓으로 안정함
+                    if($entity->distance($entity) <= 10) {  //늑대 주변에 10칸안에 다른 늑대들이 있다면 그늑대들도 때린 사람 공격
+                        $entity->setTarget($target->getId()); // AI 만들꺼에요 -푸키-
+                        $this->targetdata[$entity->getId()] = $target->getId();
+                    }
+                }
+            }
         }
     }
     
