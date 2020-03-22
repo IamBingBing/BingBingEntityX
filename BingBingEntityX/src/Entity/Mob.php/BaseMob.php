6dccf722ddad3e3cc7c083d34b859a7fb0965030
McpePuki <60613217+McpePuki@users.noreply.github.com>
2020-03-22 12:31:03 +0900

    Update BaseMob.php

--- BingBingEntityX/src/Entity/Mob.php/BaseMob.php
+++ BingBingEntityX/src/Entity/Mob.php/BaseMob.php
@@ -5,7 +5,7 @@ use pocketmine\entity\Entity;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 class BaseMob extends EntityBase{
-    public function seeing()
+    public function seeing() //푸키 AI 걷기
     {
         $x = $this->target->x - $this->x;
         $y = $this->target->y - $this->y;
@@ -25,7 +25,7 @@ class BaseMob extends EntityBase{
         
     }
 
-  public function moveing(): void
+    public function moveing(): void
     {
         $x = $this->target->x - $this->x;
         $z = $this->target->z - $this->z;
@@ -38,7 +38,19 @@ class BaseMob extends EntityBase{
         
         
     }
-    public function jumping()
+    
+       public function onUpdate(int $currentTick) : bool{
+        if ($this->target->getLevel() == $this->pos->getLevel()){
+            $this->moveing();
+            $this->jumping();
+            $this->seeing();
+        }
+        /*else { 
+            $this->teleport(new Position($this->target->x, $this->target->y, $this->target->z, $this->target->level) );
+        }*/
+    }
+    
+    public function jumping() //푸키 AI 점프
     {
         $x= $this-> x + 1 * sin($this-> getYaw());
         $z = $this-> z + 1 * cos($this-> getYaw());
@@ -46,6 +58,13 @@ class BaseMob extends EntityBase{
         if($block->getId() != 0){
             parent::jump();
             $this->jump();
+        }
+    }
+    
+      public function Attack() { //푸키 Attack부분
+        if($this->target == null) return;
+        if($this->traget->distance($this) <=  0.5) {// 타켓 엔티티 0.5 범위에 엔티티 있으면 공격 이거는 따로 몬스터 클래스안에서 하는게 간편할꺼 같습니다.
+           $this->target->attack (new EntityDamageByEntityEvent ($this, $this->target, EntityDamageByEntityEvent::CAUSE_ENTITY_ATTACK, $this->damage));
         }
     }
 
