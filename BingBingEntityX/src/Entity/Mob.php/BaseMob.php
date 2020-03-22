8187aa6fe4113b24513f5818881530c25c476251
McpePuki <60613217+McpePuki@users.noreply.github.com>
2020-03-22 12:26:20 +0900

    Update BaseMob.php

--- BingBingEntityX/src/Entity/Mob.php/BaseMob.php
+++ BingBingEntityX/src/Entity/Mob.php/BaseMob.php
@@ -7,7 +7,15 @@ use pocketmine\event\Listener;
 class BaseMob extends EntityBase{
     public function seeing()
     {
-        //TODO 몹 바라보기 구현
+        $x = $this->target->x - $this->x;
+        $y = $this->target->y - $this->y;
+        $z = $this->target->z - $this->z;
+        $abs = abs($x) + abs($z);
+        $main_xyz = sqrt(pow($x, 2) + pow($z, 2));
+        
+        $this->yaw = -atan2($x / $abs, $z / $abs) * 180 / M_PI;
+        $this->pitch = -atan2($y, $main_xyz) * 180 / M_PI;
+        $this->move ($this->motion->x, $this->motion->y, $this->motion->z);
         
     }
 
@@ -17,14 +25,28 @@ class BaseMob extends EntityBase{
         
     }
 
-    public function moveing(): void
+  public function moveing(): void
     {
-        //TODO 몹 움직임 구현
+        $x = $this->target->x - $this->x;
+        $z = $this->target->z - $this->z;
+        $main_xyz = sqrt(pow($x, 2) + pow($z, 2));
+        if($main_xyz < 1.0) return true;
+        $this->motion->x =  $this->speed *($x / $main_xyz);
+        $this->motion->z =$this->speed *($z / $main_xyz);
+        
+        $this->move ($this->motion->x, $this->motion->y, $this->motion->z);
+        
         
     }
     public function jumping()
     {
-        
+        $x= $this-> x + 1 * sin($this-> getYaw());
+        $z = $this-> z + 1 * cos($this-> getYaw());
+        $block = $this->target->level->getBlock(new Vector3($x, $this->y, $z));
+        if($block->getId() != 0){
+            parent::jump();
+            $this->jump();
+        }
     }
 
 
